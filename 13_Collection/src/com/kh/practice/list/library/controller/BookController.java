e7d426b5ac7874b795beb2d7df2915a35137295f
duoh <plan0027@gmial.com>
2020-07-30 21:50:48 +0900

    13_collection 연습 문제 풀이 중..

---
+++ 13_Collection/src/com/kh/practice/list/library/controller/BookController.java
@@ -0,0 +1,32 @@
+package com.kh.practice.list.library.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kh.practice.list.library.model.vo.Book;
+
+public class BookController {
+	private List<Book> list = new ArrayList<Book>();
+	
+	public BookController() {}
+	
+	public void insertBook() {
+		
+	}
+	
+	public ArrayList<Book> selectList() {
+		
+	}
+	
+	public ArrayList<Book> searchBook(String keyword) {
+		
+	}
+	
+	public Book deleteBook(String title, String author) {
+		
+	}
+	
+	public int ascBook() {
+		
+	}
+}
